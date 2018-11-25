c9355974022dcf1ec7948924f31ec6a9fd47764b
Rowan Cannaday <cannadayr@gmail.com>
2018-11-24 18:59:18 -0800

    infinite loop in wrapper for crawl

--- wrapper.sh
+++ wrapper.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-sqlite3 pages.db < <(autom4te -l m4sugar crawl.sql.m4)
+#TODO combine w/ cte in crawl.sql
+query="
+    select load_extension('sqlitepipe/sqlitepipe');
+    select \
+        url \
+    from whitelist \
+\
+    left outer join ( \
+        select \
+            url, \
+            printf(\"%s\",pipe('sed ''s/^.*:\/\///'' | sed ''s/\([^\/]*\)\/.*/\1/'' | awk -F\".\" ''{printf \$(NF-1) \".\" \$NF}''',page.url)) as domain \
+\
+        from page \
+\
+        where content is null
+\
+        and is_retired is not 1 \
+\
+    ) page on page.domain = whitelist.domain \
+\
+    limit 1 \
+;"
+url="$(printf "%s" "${query}" | sqlite3 pages.db | tr -d '\n')"
+
+echo "url = ${url}"
+while test -n "${url}"
+do
+    sqlite3 pages.db < crawl.sql | sqlite3 -echo pages.db
+    echo
+    sleep 1
+    url="$(printf "%s" "${query}" | sqlite3 pages.db | tr -d '\n')"
+    echo "url = ${url}"
+done

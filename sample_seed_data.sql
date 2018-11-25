2ef3b113458e8f90d9305cbd916950f8120103eb
Rowan Cannaday <cannadayr@gmail.com>
2018-11-24 19:04:32 -0800

    add sample_seed_data.sql

---
+++ sample_seed_data.sql
@@ -0,0 +1,5 @@
+-- WARNING: Use this ONLY on domains you control!
+-- no robots.txt handling very alpha etc
+insert into page (url) values ("https://www.example.com");
+
+insert into whitelist (domain) values ("example.com");

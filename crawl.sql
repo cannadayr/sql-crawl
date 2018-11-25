9c6f539bf92d4c929c0d2841ed263cae0c5f9760
Rowan Cannaday <cannadayr@gmail.com>
2018-11-24 16:40:32 -0800

    whitelisted urls cte

--- crawl.sql
+++ crawl.sql
@@ -1,5 +1,23 @@
 .load sqlitepipe/sqlitepipe
 
+with whitelisted_urls as (
+    select
+        url
+    from whitelist
+
+    left outer join (
+        select
+            url,
+            printf("%s",pipe('sed ''s/^.*:\/\///'' | sed ''s/\([^\/]*\)\/.*/\1/'' | awk -F"." ''{printf $(NF-1) "." $NF}''',page.url)) as domain
+
+        from page
+
+        where content is null -- TODO add a last_fetched field and query-by
+
+    ) page on page.domain = whitelist.domain
+
+    limit 1
+)
 select
     url
 from whitelist

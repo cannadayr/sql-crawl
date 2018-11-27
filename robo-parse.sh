e727b8260e4b8c36edf77bbeabeb9e62ea827cea
Rowan Cannaday <cannadayr@gmail.com>
2018-11-26 16:26:43 -0800

    successful robo-parse!
    still doesn't work on percent-encoded urls

--- robo-parse.sh
+++ robo-parse.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# expects no trailing backslash
-to_regex()
-{
-    # transform pattern to valid regex
-    # /  -> \/
-    # .* -> *
-    # ?  -> \?
-    # +  -> \+
-    # *  -> .*
-    # '  -> ''
-    sed 's/\//\\\//g' \
-        | sed 's/\.\*/*/g' \
-        | sed 's/\?/\\?/g' \
-        | sed 's/\+/\\+/g' \
-        | sed 's/\*/.*/g' \
-        | sed 's/\x27/\x27\x27/g'
-}
 
 domain="$1"
 
@@ -31,33 +14,53 @@ robots_txt=$(wget -qO- ${robots_url})
 # get the rules for the following user-agent sections:
 #   Lynx*, *,
 # get the disallowed paths & convert to regex pattern
+#   expects no trailing backslash
+#   transform pattern to valid regex
+#   /  -> \/
+#   .* -> *
+#   ?  -> \?
+#   +  -> \+
+#   *  -> .*
+#   '  -> ''
 # insert the rules into the db
 printf "%s" "${robots_txt}" \
     | sed 's/#.*$//g' \
     | sed '/^\s*$/d' \
     | tr '[:upper:]' '[:lower:]' \
-    | awk 'BEGIN{RS="user-agent: "} /^Lynx/{print} /^\*/{print}' \
-    | awk -v domain="${domain}" '
-        BEGIN{FS=": "}
+    | awk 'BEGIN{RS="user-agent: "} /^lynx/{print} /^\*/{print}' \
+    | awk -v domain="${domain}" \
+          -v to_regex="sed 's#\x5c/#\x5c\x5c\x5c/#g' \
+                        | sed 's#\x5c.\x5c*#*#g' \
+                        | sed 's#\x5c?#\x5c\x5c?#g' \
+                        | sed 's#\x5c+#\x5c\x5c+#g' \
+                        | sed 's#\x5c*#.*#g' \
+                        | sed 's#\x27\x5c\x27\x27#\x27\x5c\x27\x27\x27\x5c\x27\x27#g'" '\
+        BEGIN{FS=": "} \
         /^disallow/ { \
+            cmd = "printf \"%s\"" $2 " |" to_regex; \
+            cmd | getline rule_pattern; \
+            close(cmd); \
             print \
-                "insert into rule (" \
+                "insert into \"rule\" (" \
                     "whitelist_id," \
-                    "rule," \
+                    "pattern," \
                     "is_allowed" \
                 ") values (" \
                     "(select id from whitelist where domain = \x27" domain "\x27)," \
-                    "\x27" $2 "\x27, 0" \
+                    "\x27" rule_pattern "\x27, 0" \
                 ");" \
         } \
         /^allow/ { \
+            cmd = "printf \"%s\"" $2 " |" to_regex; \
+            cmd | getline rule_pattern; \
+            close(cmd); \
             print \
-                "insert into rule (" \
+                "insert into \"rule\" (" \
                     "whitelist_id," \
-                    "rule," \
+                    "pattern," \
                     "is_allowed" \
                 ") values (" \
                     "(select id from whitelist where domain = \x27" domain "\x27)," \
-                    "\x27" $2 "\x27, 1);"}'
+                    "\x27" rule_pattern "\x27, 1);"}'
 
 

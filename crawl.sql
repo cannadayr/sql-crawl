1aeb0db0cd68e9865aaa0429e0be7d485d77a939
Rowan Cannaday <cannadayr@gmail.com>
2018-11-27 09:53:50 -0800

    clean up awk templating code

--- crawl.sql
+++ crawl.sql
@@ -45,7 +45,21 @@ select
     ) then printf("%s",'update page set is_retired = 1 where url = ' || quote(url) || ';')
     else
         printf("%s",'begin transaction; update page set content = ' || quote(content) || 'where url = ' || quote(url) || ';')
-        || pipe('while IFS= read -r line; do uri-parser/uri-parser  --protocol --host "${line}" | awk ''BEGIN{ret=1} {if(NF==2){ret=0;}} END{exit ret}'' && { uri-parser/uri-parser --defragment "${line}" | awk ''{print "insert into page (url) values (\x27" $1 "\x27); insert into link (src_page_id,dest_page_id) values ((select id from page where url = \x27' || quote(url) || '\x27), (select id from page where url = \x27" $1 "\x27));" }'';}; done',links)
+        || pipe('\
+while IFS= read -r line;
+do
+    uri-parser/uri-parser  --protocol --host "${line}" \
+        | awk ''BEGIN{ret=1} {if(NF==2){ret=0;}} END{exit ret}'' && { \
+            uri-parser/uri-parser --defragment "${line}" \
+            | awk ''{print \
+                        "insert into page (url) values (\x27" $1 "\x27);" \
+                        "insert into link (src_page_id,dest_page_id) values (" \
+                            "(select id from page where url = \x27' || quote(url) || '\x27)," \
+                            "(select id from page where url = \x27" $1 "\x27)" \
+                        ");" \
+            }''; \
+        };
+done',links)
         || printf("%s",'commit;')
     end as insert_queries
 from (

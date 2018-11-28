4b1ba3c0b419920996e443bfc6e808a8ba5c7422
Rowan Cannaday <cannadayr@gmail.com>
2018-11-28 12:14:54 -0800

    wrap queries in transactions
    echo sql as we execute it

--- wrapper.sh
+++ wrapper.sh
@@ -28,33 +28,37 @@ url="$(printf "%s" "${query}" | sqlite3 pages.db | tr -d '\n')"
 # clear tmp_rank table
 # clear out_degree table
 clear_query="\
+    begin transaction; \
     delete from out_degree; \
     delete from page_rank; \
     delete from tmp_rank; \
+    commit; \
 "
 
 # computes out-degrees
 # NOTE- every page in graph must have an out-degree!
 out_degree_init=" \
+    begin transaction; \
     insert into out_degree \
     select \
         page.id, \
         count(link.src_page_id) \
     from page \
     left outer join link on link.src_page_id = page.id \
-    group by page.id \
-; \
+    group by page.id; \
+    commit; \
 "
 
 alpha="0.8"
 page_rank_init=" \
+    begin transaction; \
     insert into page_rank (id,rank) \
     select \
         page.id, \
         (1 - ${alpha}) / (select count(*) from page) as rank \
     from page \
-    inner join out_degree on out_degree.id = page.id \
-; \
+    inner join out_degree on out_degree.id = page.id; \
+    commit;
 "
 
 page_rank_compute=" \
@@ -77,33 +81,36 @@ page_rank_compute=" \
 "
 
 
-#echo "url = ${url}"
-while test -n "${url}"
+count=0
+echo "url = ${url}"
+while test -n "${url}" && test "${count}" -lt 100
 do
+    count=$(echo "${count} + 1" | bc)
+    #echo "${count}"
     sqlite3 pages.db < crawl.sql | sqlite3 -echo pages.db
     echo
-    sleep 10
-    #sleep 1
+    #TODO dynamically determine delay
+    sleep 30
 
     # clear pagerank related tables
-    printf "%s" "${clear_query}" | sqlite3 pages.db
+    printf "%s" "${clear_query}" | sqlite3 -echo pages.db
 
     # init out_degree
-    printf "%s" "${out_degree_init}" | sqlite3 pages.db
+    printf "%s" "${out_degree_init}" | sqlite3 -echo pages.db
 
     # init page_rank
-    printf "%s" "${page_rank_init}" | sqlite3 pages.db
+    printf "%s" "${page_rank_init}" | sqlite3 -echo pages.db
 
     # compute page_rank
     iter=1
 
     while test "${iter}" -lt 5
     do
-        printf "%s" "${page_rank_compute}" | sqlite3 pages.db
+        printf "%s" "${page_rank_compute}" | sqlite3 -echo pages.db
         iter=$(echo "${iter} + 1" | bc)
     done
 
-    url="$(printf "%s" "${query}" | sqlite3 pages.db | tr -d '\n')"
-    #echo "url = ${url}"
+    # check if we still have a url to crawl
+    url="$(printf "%s" "${query}" | sqlite3 -echo pages.db | tr -d '\n')"
 done
 
